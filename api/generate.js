b44b1ebbe291a600f84ab1042e2b55f43068a3d2
meteor1te1128 <Kevin68654926@gmail.com>
2026-05-10 09:20:24 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -126,6 +126,7 @@ export default async function handler(req, res) {
     );
 
     const prediction = await startRes.json();
+    console.log('Replicate response:', JSON.stringify(prediction));
 
     if (prediction.error) {
       console.error('Replicate error:', prediction.error);
